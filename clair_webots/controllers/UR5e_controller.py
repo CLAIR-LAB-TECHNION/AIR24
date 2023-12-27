f73a5a2fc34702f5ab2c33641d6dff235e40a54d
yuvalgos <yuvalgos@gmail.com>
2023-12-27 20:20:39 +0200

    typo

--- clair_webots/controllers/UR5e_controller.py
+++ clair_webots/controllers/UR5e_controller.py
@@ -7,18 +7,40 @@ picked_up_state = [- 0.6, -1.2, 1.4, -2.1, -1.4, -0.8]
 
 
 if  __name__ == '__main__':
-    ur5 = UR5eRobot(max_velocity_scale=0.5)
+    # create a robot instance, there are default arguments,
+    # read the __init__ method documentation for more information
+    ur5 = UR5eRobot()
+
+    # run tje simulation for half a second:
     ur5.simulate_seconds(0.5)
+    # you can run it for one step as well, a step is 32 milliseconds by default:
+    ur5.robot_step()
 
+    # get the current configuration of the robot, it's a 6d vector of the joint states
     state = ur5.get_current_config()
-    state = np.array(state)
+    print(state)  # will print a vector of 6 floats, corresponding to the joint angles in radians
 
-    ur5.move_to_config(box_grasp_config)
+    # move the robot by move_to_config, this method will run the simulation until the robot reaches the target
+    # config or fails. It will return True if the robot reached the target config, False otherwise.
+    res = ur5.move_to_config(box_grasp_config)
+    print(res)  # will print True if the robot reached the target config, False otherwise.
+
+    # the gripper should be now above the box, close it and run the simulation for 2 seconds to let it close:
     ur5.close_gripper(0.0)
     ur5.simulate_seconds(2)
-    ur5.scale_max_velocities(0.02)
-    ur5.move_to_config(picked_up_state)
-    ur5.simulate_seconds(2)
+    # we could have called robot.close_gripper_and_wait(0.0, 2) instead, and it would have done the same thing.
 
-    # add set config example
+    # we are going to pick up the box now, but if the robot will move to fast it will lose grasp and the box
+    # will fall, so we need to slow down the robot, we can do that by scaling the max velocities of the robot:
+    ur5.scale_max_velocities(0.02)
+    # we will now move the robot to a picked up state, but instead of using move_to_config we will run the
+    # simulation manually:
+    ur5.set_target_config(picked_up_state)
+    ur5.simulate_seconds(5)
+
+    # let's open the gripper and drop the box:
+    ur5.open_gripper_and_wait(2)
+
+    # run the simulationfor a few more seconds, then the controller will terminate and simulation will stop:
+    ur5.simulate_seconds(10)
 
